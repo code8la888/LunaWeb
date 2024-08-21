8cd64fccbfce47859c0812a8813174fa2054fc93
code8la888 <kazuhaha1997@gmail.com>
2024-08-21 09:01:48 +0800

    08210901

--- src/main/java/tw/luna/javaee/Luna01.java
+++ src/main/java/tw/luna/javaee/Luna01.java
@@ -13,11 +13,18 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class Luna01
  */
 @WebServlet("/Luna01")
+//指定servlet映射到特定的URL路徑，當訪問/Luna01時，這個servlet會被調用
 public class Luna01 extends HttpServlet {
+	//servlet是用來處理客戶端的請求並生成響應
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//doGet()方法用來處理get請求。get請求主要用於請求數據。
+		//request參數包含客戶端的請求信息，response參數用來設定回傳給客戶端的響應
+
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		System.out.println("OK");
 		PrintWriter pw= response.getWriter();
+		//PrintWriter物件用來向客戶端輸出字串，此物件與響應(response)有關，可以通過它將內容寫回客戶端。
 		pw.print("Hello, Luna");
+		//輸出"Hello,Luna"到客戶端，這將成為響應內容的一部分，並顯示在客戶端的瀏覽器。
 	}
 }
--- src/main/java/tw/luna/javaee/Luna02.java
+++ src/main/java/tw/luna/javaee/Luna02.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 public class Luna02 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
+		//用來處理get請求，request是用戶端的請求訊息，response用來設定傳回客戶端的響應		
 		Enumeration<String> headers = request.getHeaderNames();
 		while(headers.hasMoreElements()) {
 			String name= headers.nextElement();
